7ef5dba4907002b8fe9988b1e21e5bfa6cc6f898
Leo <leowu56@gmail.com>
2016-05-19 21:40:42 +0800

    ! 正确post lua-mode config

--- leo/packages.el
+++ leo/packages.el
@@ -1,4 +1,15 @@
-(setq leo-packages '(youdao-dictionary))
+(setq leo-packages '(
+                     youdao-dictionary
+                     lua-mode))
 
 (defun leo/init-youdao-dictionary ()
-  (spacemacs/set-leader-keys "oy" 'youdao-dictionary-search-at-point+))
+  (use-package youdao-dictionary
+    :init
+    (progn
+      (spacemacs/set-leader-keys "oy" 'youdao-dictionary-search))))
+
+(defun leo/post-init-lua-mode ()
+  (use-package lua-mode
+    :config
+    (progn
+      (setq lua-indent-level 4))))
