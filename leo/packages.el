076e99416d096a36891ab7f301ffe898b1f96ad4
Leo <leowu56@gmail.com>
2016-08-09 09:16:31 +0800

    + 镜像库

--- leo/packages.el
+++ leo/packages.el
@@ -29,24 +29,3 @@
       (setq c-basic-offset 4)
       (c-set-offset 'substatement-open 0)
       )))
-
-;; (defun leo/init-wttrin()
-;;   (use-package wttrin
-;;     :ensure t
-;;     :commands (wttrin)
-;;     :init
-;;     (setq wttrin-default-cities '("Nanjing" "Beijing" "Shanghai" "Bristol"))))
-
-
-(defun leo/post-init-evil ()
-  (use-package evil
-    :defer t
-    :config
-    (progn
-      (setq evil-normal-state-tag   (propertize "[N]" 'face '((:background "DarkGoldenrod2" :foreground "black")))
-            evil-emacs-state-tag    (propertize "[E]" 'face '((:background "SkyBlue2" :foreground "black")))
-            evil-insert-state-tag   (propertize "[I]" 'face '((:background "chartreuse3") :foreground "white"))
-            evil-motion-state-tag   (propertize "[M]" 'face '((:background "plum3") :foreground "white"))
-            evil-visual-state-tag   (propertize "[V]" 'face '((:background "gray" :foreground "black")))
-            evil-operator-state-tag (propertize "[O]" 'face '((:background "purple"))))
-      )))
